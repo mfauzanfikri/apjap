568355746a58577f0d32733f009c58a146310e87
mfauzanfikri <mfauzanfikri09@gmail.com>
2024-06-05 09:27:07 +0700

    update landing

--- layouts/header.php
+++ layouts/header.php
@@ -59,10 +59,8 @@
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top">
         <div class="container d-flex align-items-center">
-
-            <h1 class="logo me-auto"><a href="index.html">RSUD OI</a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+            <a href="./" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
 
             <nav id="navbar" class="navbar order-last order-lg-0">
                 <ul>
@@ -73,7 +71,7 @@
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
 
-            <a href="#appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">Make an</span> Appointment</a>
+            <a href="#appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">Buat</span> Jadwal Pemeriksaan</a>
 
         </div>
     </header><!-- End Header -->
